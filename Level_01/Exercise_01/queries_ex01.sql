3b180a9497d651b7a040a647befcefeac888cfc2
mirexan <mmregada@gmail.com>
2025-10-29 11:10:32 +0100

    feat: ex01 lvl1 finished

--- Level_01/Exercise_01/queries_ex01.sql
+++ Level_01/Exercise_01/queries_ex01.sql
@@ -12,21 +12,22 @@ GROUP BY client_name;
 
 SELECT DISTINCT sale.selling_agent,
 sale.glasses_id,
-glasses.brand AS brand,
+glasses.brand AS brand
 FROM sale
 INNER JOIN glasses
 	ON sale.glasses_id = glasses.glasses_id
 WHERE sale.selling_agent = 'Alberto Domingo'
 AND sale.sale_date BETWEEN '2025-01-01' AND '2026-01-01';
 
-SELECT DISTINCT provider.provider_name,
-glasses.brand AS brand
+SELECT provider.provider_name,
+glasses.brand AS brand,
 SUM(sale.quantity) AS Total_Sold
 FROM sale
 INNER JOIN glasses
 	ON sale.glasses_id = glasses.glasses_id
 INNER JOIN provider
     ON glasses.provider_name = provider.provider_name
+GROUP BY provider.provider_name, glasses.brand
 ;
     
 
